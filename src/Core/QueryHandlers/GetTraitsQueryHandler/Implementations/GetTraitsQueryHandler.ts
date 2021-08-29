8aa1b21070c0529700dbf36b438d42d2600f9223
kavastrophe <boroth.contact@gmail.com>
2021-08-29 15:08:34 +0800

    update trait generator

--- src/Core/QueryHandlers/GetTraitsQueryHandler/Implementations/GetTraitsQueryHandler.ts
+++ src/Core/QueryHandlers/GetTraitsQueryHandler/Implementations/GetTraitsQueryHandler.ts
@@ -6,8 +6,9 @@ import { GenerateCharacterTraitsQuery } from "Resources/Models/Queries/GenerateC
 import { IGetTraitsQueryHandler } from "../IGetTraitsQueryHandler";
 
 export class GetTraitsQueryHandler implements IGetTraitsQueryHandler {
-    Execute(query: GenerateCharacterTraitsQuery): CharacterTrait[] {
-        const numberOfTraits = GetRandomInteger(query.max, query.min);
+
+    Execute(_: GenerateCharacterTraitsQuery): CharacterTrait[] {
+        const numberOfTraits = this.GetAmountOfTraits();
         const traits = new Array<CharacterTrait>();
 
         for(var i=0; i < numberOfTraits; i++){
@@ -17,5 +18,27 @@ export class GetTraitsQueryHandler implements IGetTraitsQueryHandler {
         }
 
         return traits;
+    }
+    
+    private GetAmountOfTraits():number {
+        /**
+         * Target distribution:
+         * 2 traits: 30%
+         * 3 traits: 40%
+         * 4 traits: 20%
+         * 5 traits: 10%
+         */
+        const score = GetRandomInteger(100);
+
+        if(score > 90){
+            return 5;
+        } else if(score > 70) {
+            return 4;
+        } else if(score > 30) {
+            return 3;
+        } else {
+            return 2;
+        }
+        
     }
 }
